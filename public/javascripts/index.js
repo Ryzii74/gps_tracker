afb0080d2203c76c3e985cd3d482685b59a10ccd
Dmitri Boronin <Ryzii@yandex.ru>
2015-12-19 19:24:46 +0500

    ADD map wheel

--- public/javascripts/index.js
+++ public/javascripts/index.js
@@ -1,7 +1,7 @@
 function initMap() {
     var map = new google.maps.Map(document.getElementById('map'), {
         center: {lat: 55.15, lng: 61.384},
-        scrollwheel: false,
+        scrollwheel: true,
         zoom: 13
     });
 
