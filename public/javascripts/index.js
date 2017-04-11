172fc683df142446dd94681b1cc278d6a830180a
Ryzii74 <Ryzii@yandex.ru>
2017-04-12 00:10:33 +0300

    + eslint first fixes

--- public/javascripts/index.js
+++ public/javascripts/index.js
@@ -1,26 +1,26 @@
 function initMap() {
-    var map = new google.maps.Map(document.getElementById('map'), {
-        center: {lat: 55.15, lng: 61.384},
+    const map = new google.maps.Map(document.getElementById('map'), {
+        center: { lat: 55.15, lng: 61.384 },
         scrollwheel: true,
-        zoom: 13
+        zoom: 13,
     });
 
-    var line;
+    let line;
     showLine();
-    setInterval(function () {
+    setInterval(() => {
         line && line.setMap(null);
         showLine();
     }, 10000);
 
     function showLine() {
-        $.ajax(`/points${window.location.pathname.replace('/map', '')}`).done(function (data) {
+        $.ajax(`/points${window.location.pathname.replace('/map', '')}`).done((data) => {
             data = JSON.parse(data);
             line = new google.maps.Polyline({
                 path: data,
                 geodesic: true,
                 strokeColor: '#FF0000',
                 strokeOpacity: 1.0,
-                strokeWeight: 2
+                strokeWeight: 2,
             });
 
             line.setMap(map);
--- routes/points.js
+++ routes/points.js
@@ -1,61 +1,60 @@
-var express = require('express');
-var router = express.Router();
+const express = require('express');
+const router = express.Router();
 
 /* GET home page. */
-router.post('/add', function(req, res, next) {
+router.post('/add', (req, res, next) => {
     console.log(req.body);
 
-    var point = {
-        lat : Number(req.body.lat),
-        lng : Number(req.body.lon),
-        time : +new Date(),
-        tag : req.body.tag
+    const point = {
+        lat: Number(req.body.lat),
+        lng: Number(req.body.lon),
+        time: +new Date(),
+        tag: req.body.tag,
     };
-    global.db.collection('points').insert(point, function (err) {
+    global.db.collection('points').insert(point, (err) => {
         if (err) return console.log(err);
-
     });
 });
 
-router.get('/getLast/:tag', function(req, res, next) {
+router.get('/getLast/:tag', (req, res, next) => {
     global.db.collection('points')
         .find({
-            tag : req.params.tag
+            tag: req.params.tag,
         }, {
-            _id : 0,
-            lat : 1,
-            lng : 1
+            _id: 0,
+            lat: 1,
+            lng: 1,
         })
         .limit(1)
-        .sort({time : -1})
-        .toArray(function (err, users) {
-            var result = {};
+        .sort({ time: -1 })
+        .toArray((err, users) => {
+            let result = {};
             if (err) {
-                result = { success : false, error : err };
+                result = { success: false, error: err };
             } else {
-                result = { success : true, data : users[0] };
+                result = { success: true, data: users[0] };
             }
 
             res.write(JSON.stringify(result));
             res.end();
-    });
+        });
 });
 
-router.get('/:tag/:limit', function(req, res, next) {
+router.get('/:tag/:limit', (req, res, next) => {
     global.db.collection('points')
         .find({
-            tag : req.params.tag
+            tag: req.params.tag,
         }, {
-            lat : 1,
-            lng : 1,
-            _id : 0
+            lat: 1,
+            lng: 1,
+            _id: 0,
         })
         .limit(Number(req.params.limit) || 20)
-        .sort({ _id : -1 })
-        .toArray(function (err, points) {
+        .sort({ _id: -1 })
+        .toArray((err, points) => {
             res.write(JSON.stringify(points));
             res.end();
-    });
+        });
 });
 
 module.exports = router;
