67959c9c894c9759742887043ce170f20c233899
Ryzii74 <Ryzii@yandex.ru>
2017-04-15 23:13:19 +0300

    + body parser middleware

--- middlewares/index.js
+++ middlewares/index.js
@@ -1,15 +1,14 @@
 const logger = require('morgan');
 const cookieParser = require('cookie-parser');
-const bodyParser = require('body-parser');
 
+const bodyParserMiddleware = require('./bodyParser');
 const viewMiddleware = require('./view');
 
 module.exports = (app) => {
     viewMiddleware(app);
+    bodyParserMiddleware(app);
 
     app.use(logger('dev'));
-    app.use(bodyParser.json());
-    app.use(bodyParser.urlencoded({ extended: false }));
     app.use(cookieParser());
     app.use(express.static(path.join(__dirname, 'public')));
 };
