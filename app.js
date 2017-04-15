6f74ba5359758a437dbd642151061a672940309a
Ryzii74 <Ryzii@yandex.ru>
2017-04-15 23:08:45 +0300

    + make start script async

--- app.js
+++ app.js
@@ -2,9 +2,11 @@ const express = require('express');
 
 const app = express();
 
-require('./libs/database').init();
-require('./middlewares')(app);
-require('./routes')(app);
+(async function () {
+    await require('./libs/database').init();
+    require('./middlewares')(app);
+    require('./routes')(app);
 
-const port = process.env.PORT || 3000;
-app.listen(port, () => console.log('listen port', port));
+    const port = process.env.PORT || 3000;
+    app.listen(port, () => console.log('listen port', port));
+})();
