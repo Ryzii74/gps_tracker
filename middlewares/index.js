d60eedc3d4c8f82972670f2b856860ef381717fe
Ryzii74 <Ryzii@yandex.ru>
2017-04-15 23:15:43 +0300

    + add logger middleware

--- middlewares/index.js
+++ middlewares/index.js
@@ -1,14 +1,14 @@
-const logger = require('morgan');
 const cookieParser = require('cookie-parser');
 
 const bodyParserMiddleware = require('./bodyParser');
 const viewMiddleware = require('./view');
+const loggerParser = require('./logger');
 
 module.exports = (app) => {
     viewMiddleware(app);
     bodyParserMiddleware(app);
+    loggerParser(app);
 
-    app.use(logger('dev'));
     app.use(cookieParser());
     app.use(express.static(path.join(__dirname, 'public')));
 };
