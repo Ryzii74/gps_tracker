89b47d8b1baa60effe188620d0649b08683072e4
Ryzii74 <Ryzii@yandex.ru>
2017-04-16 23:31:01 +0300

    + cookie middleware

--- middlewares/index.js
+++ middlewares/index.js
@@ -1,15 +1,13 @@
-const cookieParser = require('cookie-parser');
-
 const bodyParserMiddleware = require('./bodyParser');
 const viewMiddleware = require('./view');
 const loggerParserMiddleware = require('./logger');
 const staticMiddleware = require('./static');
+const cookieMiddleware = require('./cookie');
 
 module.exports = (app) => {
     viewMiddleware(app);
     bodyParserMiddleware(app);
     loggerParserMiddleware(app);
     staticMiddleware(app);
-
-    app.use(cookieParser());
+    cookieMiddleware(app);
 };
